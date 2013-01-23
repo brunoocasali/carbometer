6f22998f1cf9f56d11190a4e9319c856539f0db2
Rob Pak <rob@carbonfive.com>
2013-01-23 10:55:55 -0800

    Change Statistic Factory to Respect Unique Date Range Constraint

--- spec/factories/statistic.rb
+++ spec/factories/statistic.rb
@@ -1,8 +1,10 @@
 FactoryGirl.define do
   factory :statistic do
     source      'google.com'
-    start_date  {Date.today - Post::DEFAULT_DAY_RANGE.days}
-    end_date    {Date.today}
-    visit_count {rand(1...100)}
+    sequence(:start_date) do |n|
+      Date.today - Post::DEFAULT_DAY_RANGE.days + n
+    end
+    end_date    { start_date + 1 }
+    visit_count { rand(1...100) }
   end
 end
