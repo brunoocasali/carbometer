049784e11b3030357510ba9cd060f14cbcbf8c71
Blake Benthall <blake@benthall.net>
2013-02-20 17:20:49 -0800

    Added API to report top sources (referrers)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Carbometer::Application.routes.draw do
 
   resource :dashboard
   resources :parts, only: [:index, :show]
-  resources :posts, only: [:index]
+  resources :posts, only: [:index] do
+    collection do
+      get :sources
+    end
+  end
   root to: 'content#home'
 
 end
