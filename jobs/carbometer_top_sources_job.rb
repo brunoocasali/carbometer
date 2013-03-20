b470c7503839cf55e5b8d69333a529b568b5728f
Rob Pak <rob@carbonfive.com>
2013-03-20 16:27:52 -0700

    Introduce ENV Var for Carbometric Hostname

--- jobs/carbometer_top_sources_job.rb
+++ jobs/carbometer_top_sources_job.rb
@@ -2,7 +2,9 @@ require 'typhoeus'
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  response = Typhoeus.get 'carbometer.herokuapp.com/posts/sources.json', followlocation: true
+  host = ENV['CARBOMETRICS_HOSTNAME']
+  host = 'localhost:3000' unless host
+  response = Typhoeus.get "#{host}/posts/sources.json", followlocation: true
   response_body = JSON response.body
 
   send_event 'post-leaderboard-top-sources',
--- jobs/github_activity.rb
+++ jobs/github_activity.rb
@@ -2,7 +2,9 @@ require 'typhoeus'
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '60m', :first_in => 0 do |job|
-  response = Typhoeus.get 'carbometer.herokuapp.com/contributions.json', followlocation: true
+  host = ENV['CARBOMETRICS_HOSTNAME']
+  host = 'localhost:3000' unless host
+  response = Typhoeus.get "#{host}/contributions.json", followlocation: true
 
   contributions = JSON response.body
 
