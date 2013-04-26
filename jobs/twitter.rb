a954e2c59e5fe3967b54b7918eb347d4347f3f9f
Chris Svenningsen <chris@carbonfive.com>
2013-04-26 15:30:07 -0700

    Highlight and anchor @s and #s in tweets [#48767679]

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -17,7 +17,11 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
     tweets.map! do |tweet|
       tweet_body = tweet['text']
       tweet_body.gsub!(/(.{1})(https?:\/\/[.\/\+=\w]+)([\s]?)/,"\\1<a href='\\2'>\\2</a>\\3")
+      tweet_body.gsub!(/(^|\s+)(@)(\w+)/,"\\1<a href='//twitter.com/\\3'>\\2\\3</a>")
+      tweet_body.gsub!(/(^|\s+)(#)(\w+)/,"\\1<a href='//twitter.com/search?q=%23\\3&src=hash'>\\2\\3</a>")
+
       tweet_date = Date.parse(tweet['created_at']).strftime("%d %B %Y")
+
       { name: tweet['from_user'], body: tweet_body, avatar: tweet['profile_image_url_https'], date: tweet_date }
     end
 
