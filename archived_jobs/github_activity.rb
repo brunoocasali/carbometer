33e823fdcee4b2892156730cf02df4b5f449dd90
Travis Herrick <travis@carbonfive.com>
2013-05-15 20:33:28 -0700

    Increase Frequency of Job

--- archived_jobs/github_activity.rb
+++ archived_jobs/github_activity.rb
@@ -1,7 +1,7 @@
 require 'typhoeus'
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '60m', :first_in => 0 do |job|
+SCHEDULER.every '10s', :first_in => 0 do |job|
   host = ENV['CARBOMETRICS_HOSTNAME']
   host = 'localhost:3000' unless host
   response = Typhoeus.get "#{host}/contributions.json", followlocation: true
