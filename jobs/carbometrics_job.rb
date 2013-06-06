299d1933accf69ea103729521f9d0b2eb84867fd
Rob Pak <rob@carbonfive.com>
2013-06-06 16:24:45 -0700

    Consolidate Locations Job into Single Carbometrics Job

--- jobs/carbometrics_job.rb
+++ jobs/carbometrics_job.rb
@@ -6,10 +6,26 @@ SCHEDULER.every '1m', :first_in => '10s' do |job|
   host = 'localhost:3000' unless host
 
   handle_recent_posts host
+  handle_locations host
 end
 
 def handle_recent_posts(host)
   response = Typhoeus.get "#{host}/posts/recent.json?limit=3", followlocation: true
   response_body = JSON response.body
   send_event('recent-posts', { posts: response_body })
+end
+
+def handle_locations(host)
+  locations_data = {}
+  locations = ['sf', 'la']
+  locations.each do |location|
+    response = Typhoeus.get "#{host}/api/v1/locations/#{location}/kitchens.json", followlocation: true
+    response_body = JSON response.body
+    locations_data[location] = {
+      kitchen: response_body[0]
+    }
+  end
+
+  send_event('carbonfive-fridge', locations_data)
+  send_event('carbonfive-counter', locations_data)
 end
