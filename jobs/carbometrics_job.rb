d68012bc6db2742dfabe24c4cb7835863aa41bcc
Rob Pak <rob@carbonfive.com>
2013-06-06 16:46:37 -0700

    Consolidate Post Excerpts Job into Single Carbometrics Job

--- jobs/carbometrics_job.rb
+++ jobs/carbometrics_job.rb
@@ -8,6 +8,7 @@ SCHEDULER.every '1m', :first_in => '10s' do |job|
   handle_recent_posts host
   handle_locations host
   handle_instagrams host
+  handle_post_excerpts host
 end
 
 def handle_recent_posts(host)
@@ -38,4 +39,19 @@ def handle_instagrams(host)
   images.each_with_index do |image, index|
     send_event("instagram-#{index+1}", { url: image['url'], username: image['username'] })
   end
+end
+
+def handle_post_excerpts(host)
+  response = Typhoeus.get "#{host}/posts/excerpt.json", followlocation: true
+  post = JSON response.body
+
+  send_event('blog-excerpt', {
+    post_title: post['title'],
+    post_path: post['path'],
+    post_excerpt: post['excerpt'],
+    post_author: post['author_name'],
+    post_visits: post['visit_sum'],
+    post_comments: post['comment_count'],
+    post_tweets: post['tweet_count']
+  })
 end
