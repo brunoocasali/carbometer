63f3fdf63ad6b6f7debaaaaedd6e538f0a428b33
Rob Pak <rob@carbonfive.com>
2013-06-10 17:05:28 -0700

    Update Jobs to Use New Posts API

--- jobs/carbometer_posts_job.rb
+++ jobs/carbometer_posts_job.rb
@@ -4,8 +4,8 @@ require 'typhoeus'
 SCHEDULER.every '1m', :first_in => 0 do |job|
   host = ENV['CARBOMETRICS_HOSTNAME']
   host = 'localhost:3000' unless host
-  response = Typhoeus.get "#{host}/posts.json", followlocation: true
+  response = Typhoeus.get "#{host}/api/v1/posts/summary.json", followlocation: true
   response_body = JSON response.body
 
-  send_event('post-leaderboard', { posts: response_body })
+  send_event('post-leaderboard', { posts: response_body['popular'] })
 end
